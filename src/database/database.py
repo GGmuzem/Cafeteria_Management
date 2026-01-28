afc2776412a96ed27ce5b44d42e2cb6501408d51
GGmuzem <130078962+GGmuzem@users.noreply.github.com>
2026-01-28 14:27:50 +0300

    Set up Flask app and database configuration
    Initialize Flask app and configure SQLAlchemy with SQLite.

--- src/database/database.py
+++ src/database/database.py
@@ -1,3 +1,7 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy() # Загружаем бд в код
+app = Flask(name)
+# Настройка базы данных (например, sqlite)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db
+db = SQLAlchemy(app) # Загружаем бд в код
