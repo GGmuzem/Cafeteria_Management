bef5707297d2a58e99d33391860ef609a740abde
nyKilka <reznikila23@gmail.com>
2026-01-30 17:05:09 +0300

    modified:   src/schemas.py 	modified:   src/service.py

--- src/schemas.py
+++ src/schemas.py
@@ -1,5 +1,25 @@
-"""
-Какие функции реализуются здесь:
-    
-    Здесь мы определяем правильность полученных данных от пользователей и их полноценность
-"""
+from pydantic import BaseModel, field_validator
+class StorageCreate(BaseModel):
+    name: str
+    count: int
+    type_of_product: str
+
+    @field_validator('count')
+    def count_must_be_positive(cls, v):
+        if v < 0:
+            raise ValueError('Количество не может быть отрицательным')
+        return v
+    @field_validator('type_of_product')
+    def type_of_product_must_be_in_list(cls, v):
+        if v not in ['блюдо', 'продукт']:
+            raise ValueError('Тип продукта должен быть одним из: блюдо, продукт')
+        return v
+
+class StorageDelete(BaseModel):
+    name: str
+
+    @field_validator('name')
+    def name_must_not_be_empty(cls, v):
+        if not v:
+            raise ValueError('Название не может быть пустым')
+        return v
--- src/service.py
+++ src/service.py
@@ -3,3 +3,37 @@
     Проверка условий: Хватает ли у студента денег на балансе? Есть ли продукты на складе для этого блюда?
     Транзакции: Списать деньги с кошелька, создать запись о заказе, уменьшить количество продуктов на складе.
 """
+
+
+from database.store import Storage
+from config import db
+from schemas import StorageCreate, StorageDelete
+from pydantic import ValidationError
+
+def add_product(data: dict):
+    try:
+        validated_data = StorageCreate(**data)
+        
+        new_item = Storage(
+            name=validated_data.name,
+            count=validated_data.count,
+            type_of_product=validated_data.type_of_product
+        )
+        db.session.add(new_item)
+        db.session.commit()
+        return "Товар успешно добавлен"
+
+    except ValidationError as e:
+        return f"Ошибка валидации: {e}"
+
+def delete_product(data: dict):
+    try:
+        validated_data = StorageDelete(**data)
+        item = Storage.query.filter_by(name=validated_data.name).first()
+        if not item:
+            return 'Товар не найден'
+        db.session.delete(item)
+        db.session.commit()
+        return 'Товар успешно удален'
+    except ValidationError as e:
+        return f"Ошибка валидации: {e}"
