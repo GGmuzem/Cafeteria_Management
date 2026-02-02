eb58e6d72f3dc1108905b5e67afc8cb8846969e0
gelzy788 <negm.ali@mail.ru>
2026-02-02 20:03:31 +0300

    Added admin-panel

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 import os
-from flask import request, redirect, url_for, Flask, render_template
+from flask import request, redirect, url_for, Flask, render_template, flash
 from config import app, db, login_manager
+from werkzeug.security import generate_password_hash
 from database.users import User
 from database.wallets import Wallet
 from database.history import History
@@ -68,6 +69,73 @@ def register():
         else:
             return "Ошибка регистрации"
     return render_template("register.html")
+
+@app.route("/admin")
+@login_required
+def admin_panel():
+    # TODO: Добавить проверку, что текущий пользователь - админ (например, if current_user.role != 'admin': return redirect(...))
+    if current_user.role != 'admin':
+        return redirect(url_for("account"))
+    users = User.query.all()
+    return render_template("admin_panel.html", users=users)
+
+@app.route("/admin/add_user", methods=["POST"])
+@login_required
+def admin_add_user():
+    # TODO: Проверка прав админа
+    login = request.form.get("login")
+    password = request.form.get("password")
+    role = request.form.get("role")
+    
+    if User.query.filter_by(login=login).first():
+        flash("Пользователь с таким логином уже существует")
+        return redirect(url_for("admin_panel"))
+        
+    hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+    new_user = User(login=login, password=hashed_password, role=role)
+    
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Пользователь добавлен")
+    except Exception as e:
+        db.session.rollback()
+        flash(f"Ошибка добавления: {e}")
+        
+    return redirect(url_for("admin_panel"))
+
+@app.route("/admin/update_role", methods=["POST"])
+@login_required
+def admin_update_role():
+    # TODO: Проверка прав админа
+    user_id = request.form.get("user_id")
+    new_role = request.form.get("new_role")
+    
+    user = User.query.get(user_id)
+    if user:
+        user.role = new_role
+        db.session.commit()
+        flash("Роль обновлена")
+    else:
+        flash("Пользователь не найден")
+        
+    return redirect(url_for("admin_panel"))
+
+@app.route("/admin/delete_user", methods=["POST"])
+@login_required
+def admin_delete_user():
+    # TODO: Проверка прав админа
+    user_id = request.form.get("user_id")
+    
+    user = User.query.get(user_id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        flash("Пользователь удален")
+    else:
+        flash("Пользователь не найден")
+        
+    return redirect(url_for("admin_panel"))
 
 # Страница профиля
 @app.route("/account")
