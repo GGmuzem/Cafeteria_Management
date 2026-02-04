076832751c452c0019c2878195a26c10e41de03d
Draj Rusly <drajrusly@gmail.com>
2026-02-05 02:27:32 +0300

    Добавил код Ильи в user.py Надо проверить

--- src/database/users.py
+++ src/database/users.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import secrets
 import string
 from flask_login import UserMixin
+import os
 
 # Здесь мы инициализируем и проверяем таблицу users
 
@@ -11,27 +12,33 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     login = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True) # новое
     role = db.Column(db.String(10), nullable=False)
     wallet = db.Column(db.String(255), nullable=False, unique=True)
-    key = db.Column(db.String(255), nullable=False)
-    
-    def __init__(self, login, password, role="student", wallet=None):
+    allergen = db.Column(db.String(255), nullable=True) # изменено
+    preferences = db.Column(db.String(255), nullable=True) # новое 
+
+    def __init__(self, login, password, role="student", wallet=None, health=None):
         self.login = login
         self.password = password
         self.role = role
         self.key = Fernet.generate_key().decode()
-        if wallet is None:
-            wallet = ''.join(secrets.choice(string.digits) for _ in range(16))
-        self.set_wallet(wallet)
-    
+        if wallet is None: # Если нет кошелька или создали человека то создаем ему кошелек
+            generated_wallet = ''.join(secrets.choice(string.digits) for _ in range(16))
+            self.set_wallet(generated_wallet)
+        else:
+            self.set_wallet(wallet)
+        self.health = health
+
+    key = os.getenv("KEY") # Это ключ который храниться в .env
     def set_wallet(self, wallet_value): # Шифровка кошелька
-        cipher_suite = Fernet(self.key.encode())
+        cipher_suite = Fernet(self.key)
         encrypted_text = cipher_suite.encrypt(str(wallet_value).encode('utf-8'))
         self.wallet = encrypted_text.decode('utf-8')
 
     def get_wallet(self): # Расшифровка кошелька
         try:
-            cipher_suite = Fernet(self.key.encode())
+            cipher_suite = Fernet(self.key)
             decrypted_text = cipher_suite.decrypt(self.wallet.encode('utf-8'))
             return decrypted_text.decode('utf-8')
         except Exception as e:
